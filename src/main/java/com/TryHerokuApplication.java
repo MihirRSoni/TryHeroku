cb2a782f2c05daa0302738d2200c74dc9462adcb
mihir <sonimihir07@gmail.com>
2021-09-09 12:24:16 +0530

    first commit

---
+++ src/main/java/com/TryHerokuApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryHerokuApplication.class, args);
+	}
+
+}
